574acb2032ff1198cdda817e9bc91e3b74a66200
scruz567 <120342729+scruz567@users.noreply.github.com>
2023-12-19 20:47:31 -0500

    Add files via upload

---
+++ sjf.py
@@ -0,0 +1,87 @@
+from collections import deque
+
+class Process:
+    def __init__(self, name, arrival_time, burst_time):
+        self.name = name
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+        
+def sjf_scheduler(processes):
+    # Sort the tasks by their arrival time
+    completed_processes = []
+    time = 0
+    waiting_time = [0] * len(processes)
+    completion_times = [0] * len(processes)
+    ready_queue = deque()
+    current_process = None
+
+    while len(completed_processes) < len(processes):
+        # Check for new arrivals
+        for process in processes:
+            if process.arrival_time <= time and process not in completed_processes and process not in ready_queue:
+                if process != current_process:
+                    ready_queue.append(process)
+    
+        if current_process:    
+            ready_queue.append(current_process)
+    
+        if ready_queue:
+            current_process = ready_queue.popleft()
+            
+        if current_process:
+            if ready_queue:
+                min_process = min(ready_queue, key=lambda obj: obj.burst_time)
+                if min_process.burst_time < current_process.burst_time:
+                    ready_queue.append(current_process)
+                    time += min_process.burst_time
+                    completion_times[processes.index(min_process)] = time
+                    waiting_time[processes.index(min_process)] += time - min_process.arrival_time - min_process.burst_time
+                    completed_processes.append(min_process)
+                    ready_queue.remove(min_process)
+                    current_process = None
+                else:
+                    time += current_process.burst_time
+                    completion_times[processes.index(current_process)] = time
+                    waiting_time[processes.index(current_process)] += time - current_process.arrival_time - current_process.burst_time
+                    completed_processes.append(current_process)
+                    current_process = None
+            else:
+                time += current_process.burst_time
+                completion_times[processes.index(current_process)] = time
+                waiting_time[processes.index(current_process)] += time - current_process.arrival_time - current_process.burst_time
+                completed_processes.append(current_process)
+                current_process = None
+        else:
+            time += 1
+    
+    turnaround_time = [burst_time + wait_time for burst_time, wait_time in zip([p.burst_time for p in processes], waiting_time)]
+    avg_turnaround_time = sum(turnaround_time) / len(processes)
+    avg_waiting_time = sum(waiting_time) / len(processes)
+    schedule_length = time
+    throughput = len(processes) / schedule_length
+
+    return completion_times, turnaround_time, avg_turnaround_time, waiting_time, avg_waiting_time, schedule_length, throughput
+
+def main():
+    n = int(input("Enter the number of processes: "))
+    processes = []
+
+    for i in range(n):
+        name = input(f"Enter the name of process {i + 1}: ")
+        arrival_time = int(input(f"Enter the arrival time for {name} (ms): "))
+        burst_time = int(input(f"Enter the burst time for {name} (ms): "))
+        processes.append(Process(name, arrival_time, burst_time))
+
+    completion_time, turnaround_time, avg_turnaround_time, waiting_time, avg_waiting_time, schedule_length, throughput = sjf_scheduler(processes)
+
+    print("Process\tCompletion Time\tTurnaround Time\tWaiting Time")
+    for i, process in enumerate(processes):
+        print(f"{process.name}\t{completion_time[i]}\t{turnaround_time[i]}\t{waiting_time[i]}")
+
+    print(f"Average Turnaround Time: {avg_turnaround_time}")
+    print(f"Average Waiting Time: {avg_waiting_time}")
+    print(f"Schedule Length: {schedule_length}")
+    print(f"Throughput: {throughput:.2f} processes/ms")
+
+if __name__ == "__main__":
+    main()
